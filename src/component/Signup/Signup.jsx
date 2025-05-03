ed54658c002f857ba7f82793ae9d008921b9a0cd
mbiplobmollah <biplobmollah16@gmail.com>
2025-05-04 05:38:01 +0600

    learn about regular  expression and password validation

--- src/component/Signup/Signup.jsx
+++ src/component/Signup/Signup.jsx
@@ -1,23 +1,53 @@
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import React, { useState } from 'react';
 import { auth } from '../../firebase.init';
+import { FaEye } from "react-icons/fa6";
+import { IoMdEyeOff } from "react-icons/io";
 
 const Signup = () => {
 
-    const [errorMessage, setErrorMessage] = useState('')
+  const [success, setSuccess] = useState(false);
+
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const [showPassword, setShowPassword] = useState(false);
 
     const handleSignUp = (e) =>{
         e.preventDefault();
         const email = e.target.email.value;
         const password = e.target.password.value;
-        console.log(email,password)
+        const terms = e.target.terms.checked
+        console.log(email,password, terms)
+        
+
+        setSuccess(false)
 
         setErrorMessage('')
+
+
+        if(!terms){
+          setErrorMessage('Please accept Our Terms and Conditions')
+          return
+        }
+
+
+        // validate password 
+
+        const passwordRegExp = /(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,}/ ;
+
+        if(!passwordRegExp.test(password)){
+            setErrorMessage('Password must have one lowercase, one uppercase, one digit and 6 character or longer')
+
+            return;
+        }
 
         // create user
 
         createUserWithEmailAndPassword(auth,email,password)
-        .then(result=> console.log(result))
+        .then(result=> {
+          console.log(result)
+          setSuccess(true)
+        })
         .catch(error => {
             console.log(error)
             setErrorMessage(error.message)
@@ -33,14 +63,30 @@ const Signup = () => {
           <label className="label">Email</label>
           <input name='email' type="email" className="input" placeholder="Email" />
           <label className="label">Password</label>
-          <input name='password' type="password" className="input" placeholder="Password" />
+          <div className='relative'>
+            <input 
+            name='password' 
+            type={showPassword ? 'text' : 'password'} 
+            className="input" 
+            placeholder="Password" />
+            <button 
+            onClick={()=>{setShowPassword(!showPassword)}}
+            className='btn-xs absolute top-3 right-8'>{showPassword ? <IoMdEyeOff /> : <FaEye />}</button>
+          </div>
           <div><a className="link link-hover">Forgot password?</a></div>
+          <label className="label">
+           <input type="checkbox" name='terms' defaultChecked className="checkbox" />
+              Accept Terms and Conditions
+             </label>
           <div className='text-center'>
           <button className="btn btn-neutral mt-4">Sign Up</button>
           </div>
         </form>
         {
             errorMessage && <p className='text-red-500'>{errorMessage}</p>
+        }
+        {
+          success && <p className='text-green-500'>User has created successfully</p>
         }
       </div>
     </div>
