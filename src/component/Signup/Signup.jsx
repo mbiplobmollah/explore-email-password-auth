f5a975ebea3ce7af2102cedc4b305ebcc7456fb3
mbiplobmollah <biplobmollah16@gmail.com>
2025-05-05 05:35:04 +0600

    authentication learn is done

--- src/component/Signup/Signup.jsx
+++ src/component/Signup/Signup.jsx
@@ -1,8 +1,9 @@
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, sendEmailVerification, updateProfile } from 'firebase/auth';
 import React, { useState } from 'react';
 import { auth } from '../../firebase.init';
 import { FaEye } from "react-icons/fa6";
 import { IoMdEyeOff } from "react-icons/io";
+import { Link } from 'react-router';
 
 const Signup = () => {
 
@@ -16,8 +17,10 @@ const Signup = () => {
         e.preventDefault();
         const email = e.target.email.value;
         const password = e.target.password.value;
+        const name = e.target.name.value;
+        const photo = e.target.photo.value;
         const terms = e.target.terms.checked
-        console.log(email,password, terms)
+        console.log(email,password, terms,name,photo)
         
 
         setSuccess(false)
@@ -46,7 +49,24 @@ const Signup = () => {
         createUserWithEmailAndPassword(auth,email,password)
         .then(result=> {
           console.log(result)
-          setSuccess(true)
+          // email verify
+          sendEmailVerification(auth.currentUser)
+          .then(()=>{
+            setSuccess(true)
+            alert('We sent you a verification email please check your email')
+          })
+
+          // update user profile
+          const profile = {
+            displayName: name,
+            photoURL: photo
+          }
+
+          updateProfile(auth.currentUser, profile)
+          .then(()=>{
+            console.log('user profile updated')
+          })
+          .catch(error=> console.log(error))
         })
         .catch(error => {
             console.log(error)
@@ -60,6 +80,10 @@ const Signup = () => {
       <div className="card-body">
       <h1 className="text-5xl font-bold text-center">Sign Up</h1>
         <form onSubmit={handleSignUp} className='space-y-3'>
+          <label className="label">Name</label>
+          <input name='name' type="text" className="input" placeholder="Your Name" />
+          <label className="label">Photo URL</label>
+          <input name='photo' type="text" className="input" placeholder="Photo Url" />
           <label className="label">Email</label>
           <input name='email' type="email" className="input" placeholder="Email" />
           <label className="label">Password</label>
@@ -73,7 +97,6 @@ const Signup = () => {
             onClick={()=>{setShowPassword(!showPassword)}}
             className='btn-xs absolute top-3 right-8'>{showPassword ? <IoMdEyeOff /> : <FaEye />}</button>
           </div>
-          <div><a className="link link-hover">Forgot password?</a></div>
           <label className="label">
            <input type="checkbox" name='terms' defaultChecked className="checkbox" />
               Accept Terms and Conditions
@@ -82,6 +105,7 @@ const Signup = () => {
           <button className="btn btn-neutral mt-4">Sign Up</button>
           </div>
         </form>
+        <p>Already have and account? Please <Link className='text-blue-500 underline' to={'/login'}>Login</Link></p>
         {
             errorMessage && <p className='text-red-500'>{errorMessage}</p>
         }
